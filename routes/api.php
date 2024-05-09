aa4989c3d39a5cb98983ff8b5c29a23aca74ae7d
Amir <a.moharami@gmail.com>
2024-05-09 10:21:23 +0330

    add article and comment resource

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('articles', AuthController::class);
+    Route::apiResource('articles.comments', CommentController::class);
 });
